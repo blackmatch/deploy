2bdc972f319620d1e7b6cac9a00129f71d31e708
blackmatch <coder.blackmatch@gmail.com>
2020-08-24 20:22:33 +0800

    feat: docker install

---
+++ centos/install_docker.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# doc: https://docs.docker.com/engine/install/centos/
+
+echo "begin install docker..."
+
+# uninstall old version
+yum remove docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-engine
+
+# Install using the repository
+yum install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo 
+
+yum update -y
+
+wget https://download.docker.com/linux/centos/7/x86_64/stable/Packages/containerd.io-1.2.6-3.3.el7.x86_64.rpm
+yum -y install ./containerd.io-1.2.6-3.3.el7.x86_64.rpm
+
+yum install -y docker-ce docker-ce-cli
+
+systemctl start docker
+
+systemctl enable docker
+
+dockerVersion=`docker -v`
+echo "docker version: $dockerVersion"
+
+echo "begin install docker-compose..."
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+latestVersion=`get_latest_release "docker/compose"`
+echo "docker-compose version: $latestVersion"
+
+curl -L "https://github.com/docker/compose/releases/download/$latestVersion/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+echo "all done!"
